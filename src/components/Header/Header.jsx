59c696d703a7e7e84521d22af578de89fc3f89c6
codingwolf-at <atameshwari9@gmail.com>
2022-02-10 11:52:01 +0530

    feat: created home page and added header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+export const Header = () => {
+  return (
+    <header className="nav nav-dark">
+      <span className="nav-brand">WebTube</span>
+      <div className="nav-link-container">
+          <a href="#" className="nav-link">Home</a>
+          <a href="#" className="nav-link">Contact</a>
+          <a href="#" className="nav-link">Services</a>
+      </div>
+    </header>
+  );
+}
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import "./Home.css"
+import { Header } from "../../components"
+
+export const Home = () => {
+  return (
+    <div>
+      <Header />
+      <h1>Home</h1>
+    </div>
+  )
+}
