406e5ad4aca459254cd85f843e6d166c56e74d6d
codingwolf-at <atameshwari9@gmail.com>
2022-02-21 09:59:03 +0530

    created mock DB

---
+++ src/data/mockDB.js
@@ -0,0 +1,67 @@
+export const videoList = [{
+    videoId: "Sh6lK57Cuk4",
+    title: "The Weird History of JavaScript",
+    description: "The history of JavaScript over the last 25 years. How did a simple scripting language for Netscape evolve into the world's most widely used programming language? ",
+    topic: "JavaScript",
+    channel: {
+      name: "Fireship",
+      channelId: "UCsBjURrPoezykLs9EqgamOA"
+    },
+    liked: false
+  },
+  {
+    videoId: "FSs_JYwnAdI",
+    title: "JavaScript, How It's Made",
+    description: "Learn the computer science 🔬🤓 behind #JavaScript by looking at how it works under the hood. Understand why JS is called a 'high-level single-threaded, garbage-collected, interpreted (or just-in-time compiled),  prototype-based, multi-paradigm, dynamic language with a non-blocking event loop'.",
+    topic: "JavaScript",
+    channel: {
+      name: "Fireship",
+      channelId: "UCsBjURrPoezykLs9EqgamOA",
+    },
+    liked: false
+  },
+  {
+    videoId: "9emXNzqCKyg",
+    title: "The JavaScript Survival Guide",
+    description: "The JavaScript Survival Guide 🧟 provides protection from the so-called 'weird' features that you will encounter as a JS developer. This primer will introduce you to variables, functions, objects, closures, hoisting, this-binding, and more.",
+    topic: "JavaScript",
+    channel: {
+      name: "Fireship",
+      channelId: "UCsBjURrPoezykLs9EqgamOA",
+    },
+    liked: false
+  },
+  {
+    videoId: "gigtS_5KOqo",
+    title: "JavaScript Function - What's your Function?",
+    description: "Master the basics of JavaScript Functions 🚂 in a fun tutorial packed full of 90's nostalgia. ",
+    topic: "JavaScript",
+    channel: {
+      name: "Fireship",
+      channelId: "UCsBjURrPoezykLs9EqgamOA",
+    },
+    liked: false
+  },
+  {
+    videoId: "napDjGFjHR0",
+    title: "An Encounter with JavaScript Objects",
+    description: "Part five of the full #JavaScript course 🐇🕳️. Learn everything you need to know about the JS Object ",
+    topic: "JavaScript",
+    channel: {
+      name: "Fireship",
+      channelId: "UCsBjURrPoezykLs9EqgamOA",
+    },
+    liked: false
+  },
+  {
+    videoId: "f0DrPLKf6Ro",
+    title: "The Future of JavaScript - New Features and Disruptive Trends in 2020",
+    description: "Take a look at the future of JavaScript in the year 2020. Expect awesome new language features and disruptive trends that could change everything! ",
+    topic: "JavaScript",
+    channel: {
+      name: "Fireship",
+      channelId: "UCsBjURrPoezykLs9EqgamOA",
+    },
+    liked: false
+  }
+]
