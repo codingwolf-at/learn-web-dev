82ee3febcf56c218ed83bce2881895ca5fdc0f2d
Atul Tameshwari <atameshwari9@gmail.com>
2022-03-30 23:37:53 +0530

    feat: added like functionality and some design changes

--- src/pages/VideoPage/VideoPage.jsx
+++ src/pages/VideoPage/VideoPage.jsx
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHandPointLeft, faHeart } from "@fortawesome/free-regular-svg-icons";
 import YouTubePlayer from "react-player/youtube";
 import { Link, useParams } from "react-router-dom";
 import { useLibrary } from "../../hooks";
@@ -5,13 +7,19 @@ import "./VideoPage.css";
 
 export const VideoPage = () => {
   const { videoId } = useParams();
-  const { library } = useLibrary();
+  const { library, dispatch } = useLibrary();
   const video = library.find(video => video.videoId === videoId);
+
+  const likeHandler = (videoId) => dispatch({ type: "LIKE_VIDEO", payload: videoId }); 
 
   return (
     <div className="video-page">
-      <h1>{video.title}</h1>
-      <Link to="/">Go Back</Link>
+      <Link to="/">
+        <button className="btn">
+          <FontAwesomeIcon icon={faHandPointLeft} size="lg" />
+          <span>Go Back</span>
+        </button>
+      </Link>
       <div className="player-wrapper">
         <YouTubePlayer
           className="react-player"
@@ -20,6 +28,14 @@ export const VideoPage = () => {
           width="100%"
           height="100%"
         />
+      </div>
+      <div className="video-page-content">
+        <button className={video.liked ? "btn btn-danger" : "btn"} onClick={() => likeHandler(video.videoId)}>
+          <FontAwesomeIcon icon={faHeart} size="lg" />
+          <span>{video.liked ? "Liked" : "Like"}</span>
+        </button>
+        <h1 className="display-5">{video.title}</h1>
+        <p className="lead">{video.description}</p>
       </div>
     </div>
   );
