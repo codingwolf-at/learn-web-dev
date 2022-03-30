b994392f1c2fe843bdb8ef051d95dfe0b1e66de0
Atul Tameshwari <atameshwari9@gmail.com>
2022-03-30 22:59:15 +0530

    style: tweaked thumbnail component

--- src/components/Thumbnail/Thumbnail.jsx
+++ src/components/Thumbnail/Thumbnail.jsx
@@ -3,9 +3,12 @@ import "./Thumbnail.css";
 
 export const Thumbnail = ({ video }) => {
   return (
-    <Link to={`/video/${video.videoId}`}>
+    <Link to={`/video/${video.videoId}`} className="thumbnail-link">
       <div key={video.videoId}>
         <img src={`https://img.youtube.com/vi/${video.videoId}/maxresdefault.jpg`} alt="thumbnail" className="img-thumbnail" />
+        <div className="thumbnail-content">
+          <span>{video.title}</span>
+        </div>
       </div>
     </Link>
   );
