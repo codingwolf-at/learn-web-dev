e8e04df6eda46a6c67754c1ef7363ea28b78e4f1
Atul Tameshwari <atameshwari9@gmail.com>
2022-05-03 21:58:33 +0530

    feat: created constants files

---
+++ src/constants/roadmapList.js
@@ -0,0 +1,23 @@
+import frontendImg from "../assets/frontend.jpg";
+import reactImg from "../assets/react.jpg";
+import angularImg from "../assets/angular.jpg";
+import devopsImg from "../assets/devops.jpg";
+
+export const roadmapList = [
+  {
+    title: "Frontend Roadmap",
+    img: frontendImg
+  }, 
+  {
+    title: "React Roadmap",
+    img: reactImg
+  }, 
+  {
+    title: "Angular Roadmap",
+    img: angularImg
+  },
+  {
+    title: "Devops Roadmap",
+    img: devopsImg
+  }
+];
