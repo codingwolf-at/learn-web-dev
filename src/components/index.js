20153782e0d2e630e6775b82550c6be45b8b0eb2
Atul Tameshwari <atameshwari9@gmail.com>
2022-05-03 19:03:02 +0530

    removed SideBar component & some redundant code

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
 export { Header } from "./Header/Header";
-export { SideBar } from "./SideBar/SideBar";
 export { Thumbnail } from "./Thumbnail/Thumbnail";
 export { Chip } from "./Chip/Chip";
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,11 @@
 import "./Home.css"
-import { Header, SideBar } from "../../components"
+import { Header } from "../../components"
 import { Outlet } from "react-router-dom"
 
 export const Home = () => {
   return (
     <div>
       <Header />
-      {/* <SideBar /> */}
       <Outlet />
     </div>
   )
